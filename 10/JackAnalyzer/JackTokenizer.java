4e14549e6866425497798ad0ac3c7d4d458c718f
Adrian Jewell <adrianjewell91@gmail.com>
2019-11-02 17:10:12 -0400

    first three tokens

--- 10/JackAnalyzer/JackTokenizer.java
+++ 10/JackAnalyzer/JackTokenizer.java
@@ -3,12 +3,16 @@ package JackAnalyzer;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 class JackTokenizer {
     private Scanner scanner;
     private String line;
     private String token;
+    private String tokenType;
 
     JackTokenizer(String p) throws FileNotFoundException {
         scanner = new Scanner(new File(p));
@@ -20,21 +24,40 @@ class JackTokenizer {
         return (line != null && line.length() > 0);
     }
 
-    public String advance() { 
+    public String getTokenType() {
+        return tokenType;
+    }
+    public String advance() {
         token = _nextToken();
-        if (_shouldGetNextLine()) _nextLine();
+        if (_shouldGetNextLine())
+            _nextLine();
         return token;
     }
 
     private String _nextToken() {
-        // This logic is to get the next token, now the fun begins.
         // KEYWORD, SYMBOL, IDENTIFIER, INT_CONST, STRING_CONST
+        Pattern keyword = Pattern.compile("^class|^constructor|^function|^method|^field|^static|^var|^int|^char|^boolean|^void|^true|^false|^null|^this|^let|^do|^if|^else|^while|^return");
+        Matcher keywordMatcher = keyword.matcher(line);
+        List<String> symbols = Arrays.asList("{","}","(",")","[","]",".",",",";","+","-","*","/","&","|","<",">","=","~");
+    
 
-        // Check each of these, maybe use regex, 
-        // ie. For keyword, check if the first n characters match keyword, if so 
-        // substring those n characters and assign token. 
-            //             'class' | 'constructor' | 'function' | 'method' | 'field' | 'static' | 'var' |
-            // 'int' | 'char' | 'boolean' | 'void' | 'true' | 'false' | 'null' | 'this' | 'let' | 'do' | 'if' | 'else' | 'while' | 'return'
+        if (keywordMatcher.find()) {
+            token = line.substring(0, keywordMatcher.end());
+            tokenType = "KEYWORD";
+            line = line.substring(keywordMatcher.end());
+        } else if (symbols.contains(line.substring(0,1))) {
+            token = line.substring(0, 1);
+            tokenType = "SYMBOL";
+            line = line.substring(1);
+        } else if (parseIntConstant() > 0) {
+            Integer i = parseIntConstant();
+            token = line.substring(0, i);
+            tokenType = "INT_CONST";
+            line = line.substring(i);
+        } else {
+            token = line.substring(0, 1);
+            line = line.substring(1);
+        }
         // For symbol same idea,
 
         // For int constant: check if first n characters upto 5 comprise digits less than 32768, and the 
@@ -44,9 +67,6 @@ class JackTokenizer {
 
         // Else= it's an identifier, as long at it doesn't start with a digit, which I probably don't have to check. 
 
-        // 
-        token = line.substring(0, 1);
-        line = line.substring(1);
         return token;
     }
 
@@ -62,5 +82,18 @@ class JackTokenizer {
 
     private Boolean _shouldGetNextLine() {
         return line.length() == 0 && scanner.hasNextLine();
+    }
+
+    private Integer parseIntConstant() {
+        for (int i = 5; i > 0; i--) {
+            try {
+                String s = line.substring(0,i);
+                if (Integer.parseInt(s) < 32768) return i;
+            } catch(Exception e) {
+
+            } finally {}
+        }
+
+        return 0;
     }
 }
