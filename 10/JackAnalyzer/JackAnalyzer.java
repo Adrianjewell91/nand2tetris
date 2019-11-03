ba4bf8c2a8ebf0b76f1ea8abc390e81eccf9060a
Adrian Jewell <adrianjewell91@gmail.com>
2019-11-03 12:19:36 -0500

    refactor

--- 10/JackAnalyzer/JackAnalyzer.java
+++ 10/JackAnalyzer/JackAnalyzer.java
@@ -10,35 +10,39 @@ class JackAnalyzer {
         //Tokenizer Tests.
         String[] files = new String[] {
                 "/Users/adrianjewell/Desktop/nand2tetris/projects/10/ArrayTest/Main.jack"};
-        String[] outputs = new String[] {"/Users/adrianjewell/Desktop/nand2tetris/projects/10/Test/ArrayTestT.xml"};
-        compile(files, outputs);
+        String[] tokenOutputs = new String[] {"/Users/adrianjewell/Desktop/nand2tetris/projects/10/Test/ArrayTestT.xml"};
+        String[] outputs = new String[] {"/Users/adrianjewell/Desktop/nand2tetris/projects/10/Test/ArrayTest.xml"};
+        compile(files, tokenOutputs, outputs);
 
         files = new String[] {
                 "/Users/adrianjewell/Desktop/nand2tetris/projects/10/Square/Main.jack",
                 "/Users/adrianjewell/Desktop/nand2tetris/projects/10/Square/Square.jack",
                 "/Users/adrianjewell/Desktop/nand2tetris/projects/10/Square/SquareGame.jack", };
-        outputs = new String[] { "/Users/adrianjewell/Desktop/nand2tetris/projects/10/Test/SquareMainT.xml",
+        tokenOutputs = new String[] { "/Users/adrianjewell/Desktop/nand2tetris/projects/10/Test/SquareMainT.xml",
                 "/Users/adrianjewell/Desktop/nand2tetris/projects/10/Test/SquareT.xml",
                 "/Users/adrianjewell/Desktop/nand2tetris/projects/10/Test/SquareGameT.xml", };
-        compile(files, outputs);
+        outputs = new String[] { "/Users/adrianjewell/Desktop/nand2tetris/projects/10/Test/SquareMain.xml",
+                "/Users/adrianjewell/Desktop/nand2tetris/projects/10/Test/Square.xml",
+                "/Users/adrianjewell/Desktop/nand2tetris/projects/10/Test/SquareGame.xml", };
+        compile(files, tokenOutputs, outputs);
     }
 
-    public static void compile(String[] files, String[] outputs) throws IOException {
+    public static void compile(String[] files, String[] tokenOutputs, String[] outputs) throws IOException {
         for (int i = 0; i < files.length; i++) {
-            writer = new Writer(outputs[i]);
-            tokenizer = new JackTokenizer(files[i]);
-            // Compilation engine code goes here.
-            writer.writeToken("<tokens>");
-            translate();
-            writer.writeToken("</tokens>");
-            writer.close();
+            tokenize(files[i], tokenOutputs[i]);
+            // parse(tokenOutputs[i], outputs[i]);
         }
     }
 
-    public static void translate() throws IOException {
+    public static void tokenize(String path, String output) throws IOException {
+        writer = new Writer(output);
+        tokenizer = new JackTokenizer(path);
+        writer.writeToken("<tokens>");
         while (tokenizer.hasMoreTokens()) {
             tokenizer.advance();
             writer.writeToken(tokenizer.toXML());
         }
+        writer.writeToken("</tokens>");
+        writer.close();
     }
 }
